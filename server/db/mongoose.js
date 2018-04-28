686de0a659049c5c8b91eeb2f3eb7595b3912987
Cody Eddins <cpeddins94@hotmail.com>
2018-04-28 11:40:48 -0400

    reverted back to original file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://<cody>:<bhbf2423>@ds123556.mlab.com:23556/todoapp102'
-};
-mongoose.connect(db.mlab || db.localhost);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
