e5497b2b5867a9f797bba1da14f774328fd84704
Cody Eddins <cpeddins94@hotmail.com>
2018-04-28 11:37:58 -0400

    configure mongoosedb file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp' || process.env.MONGODB_URI);
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://<cody>:<bhbf2423>@ds123556.mlab.com:23556/todoapp102'
+};
+mongoose.connect(db.mlab || db.localhost);
 
 module.exports = {mongoose};
